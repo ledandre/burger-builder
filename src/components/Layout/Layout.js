313070bba531ece6888ad17b3e5fc470f9fd3ed4
Leandro Tomaz Andre <leandro.andre@resourceit.com>
2019-08-05 15:58:17 -0300

    Adicionando estrutura e montando layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Auxilliary';
-import classes from './Layout.css';
+import './Layout.css';
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
+        <main className="content">
             {props.children}
         </main>
     </Aux>
