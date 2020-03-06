0457029bf7df7aa9da0fab9dedbf2265ab777bd4
leandro.andre <leandro.andre@buscapecompany.com>
2020-03-06 16:14:25 -0300

    Adicionando controles

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,20 @@ import BurgerIngredient from './BurgerIngredient/BurguerIngredient';
 import './Burger.css';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(ingredientKey => {
       return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
         return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
       });
-    });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
 
+    if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+  
   return (
     <div className="Burger">
       <BurgerIngredient type="bread-top" />
